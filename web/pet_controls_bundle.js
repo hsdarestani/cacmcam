2ba8b93b3784966f65da67bba95df25b1f9daf9e
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-06 17:58:46 +0330

    Load CamCam viewer runtime v2 after reliability layer

--- web/pet_controls_bundle.js
+++ web/pet_controls_bundle.js
@@ -1,5 +1,5 @@
 (function(){
 'use strict';
 function load(src,done){const s=document.createElement('script');s.src=src;s.async=false;s.onload=()=>done&&done();s.onerror=()=>done&&done();document.head.appendChild(s)}
-load('/static/pet_controls_visual.js?v=5',()=>load('/static/pet_runtime_fix.js?v=5'));
+load('/static/pet_controls_visual.js?v=8',()=>load('/static/pet_runtime_fix.js?v=8',()=>load('/static/pet_runtime_v2.js?v=8')));
 })();
