6dcd6e4a49111aa1fb17f5c5368c2ec67c76eea5
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-07 15:54:41 +0330

    Load camera runtime v3 for reliable control feedback

--- web/pet_camera_bundle.js
+++ web/pet_camera_bundle.js
@@ -8,5 +8,5 @@ function load(src,done){
   s.onerror=()=>done&&done();
   document.head.appendChild(s);
 }
-load('/static/pet_camera_visual.js?v=7',()=>load('/static/pet_camera_runtime_v2.js?v=7'));
+load('/static/pet_camera_visual.js?v=8',()=>load('/static/pet_camera_runtime_v2.js?v=8',()=>load('/static/pet_camera_runtime_v3.js?v=8')));
 })();
