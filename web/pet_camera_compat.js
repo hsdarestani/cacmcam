cc9cb70b1d26030dc8358d393e741835d2559928
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-12 11:06:50 +0330

    Improve landing and eliminate repeat app loading delays

--- web/pet_camera_compat.js
+++ web/pet_camera_compat.js
@@ -1,5 +1,5 @@
 (function(){
   function load(src){const script=document.createElement('script');script.src=src;script.async=false;document.head.appendChild(script)}
-  load('/static/pet_camera_runtime.js?v=20260911');
-  load('/static/pet_camera_ui.js?v=20260911');
+  load('/static/pet_camera_runtime.js?v=20260912-1');
+  load('/static/pet_camera_ui.js?v=20260912-1');
 })();
